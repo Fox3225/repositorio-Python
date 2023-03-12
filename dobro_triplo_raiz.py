38a844451c159fdeceefb7a1362a349b2abb919c
Fox3225 <108087168+Fox3225@users.noreply.github.com>
2023-03-11 23:15:58 -0300

    O dobro, triplo e raiz de um numero inteiro

---
+++ dobro_triplo_raiz.py
@@ -0,0 +1,8 @@
+#dobro, triplo e raiz de um numero inteiro
+n = int(input('Digite um numero'))
+d = n * 2
+t = n * 3
+r = int (n ** (1/2))
+print(f'o dobro de {n} vale {d}')
+print(f'o triplo de {n} vale {t}')
+print(f'a raiz de {n} é {r}')
