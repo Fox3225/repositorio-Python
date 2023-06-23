024af701331759298613af04f1792d548a2f559e
Carlos Pires <108087168+Fox3225@users.noreply.github.com>
2023-06-23 12:59:31 -0300

    contar_palavra_lista.py
    Ao executar esse código, ele contará quantas vezes a palavra "Tatá" aparece na lista e exibirá o resultado na saída. No caso da lista fornecida, a palavra "Tatá" aparece 5 vezes.

---
+++ contar_palavra_lista.py
@@ -0,0 +1,4 @@
+lista = ["O", "seu", "Tatá", "tá?", "Não", "o", "seu", "Tatá", "não", "tá", "mas", "a", "mulher", "do", "seu", "Tatá", "tá", ".", "E", "quando", "a", "mulher", "do", "seu", "Tatá", "tá", "é", "a", "mesma", "coisa", "que", "o", "seu", "Tatá", "tá", "tá?"]
+
+quantidade = lista.count("Tatá")
+print("A palavra 'Tatá' aparece", quantidade, "vezes na lista.")
